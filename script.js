2b479996a2dc1785d13720a006830cfcaedb53a4
darya <edinadk@mer.ci.nsu.ru>
2022-04-02 12:49:09 +0700

    Мы все сделали

--- script.js
+++ script.js
@@ -4,26 +4,34 @@ window.onload = function initRemoveButtons() {
     for (let i = 0; i < todoList.length; i++) {
         const span = document.createElement("span");
         const btn = document.createTextNode("\u00D7");
+
         span.className = "close";
         span.appendChild(btn);
         todoList[i].appendChild(span);
     }
 
     const close = document.getElementsByClassName("close");
-
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            const div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-
     const list = document.querySelector('ul');
+
     list.addEventListener('click', function(ev) {
         if (ev.target.tagName === 'LI') {
             ev.target.classList.toggle('checked');
+            updateCounters();
         }
-    }, false);
+        else if (ev.target.tagName === "SPAN") {
+            let div = ev.target.parentNode;
+            div.remove();
+            updateCounters();
+    }}, false);
+
+}
+
+function handleKeyPress(e){
+    const key=e.keyCode || e.which;
+
+    if (key === 13){ // Клавиша Enter
+        addToList();
+    }
 }
 
 // Добавление дела
@@ -31,13 +39,19 @@ function addToList() {
     const li = document.createElement("li");
     const inputValue = document.getElementById("input").value;
     const t = document.createTextNode(inputValue);
+
     li.appendChild(t);
 
-    document.getElementById("list").appendChild(li);
+    if (inputValue === '') {
+        alert("Дело пустым не бывает");
+    } else {
+        document.getElementById("list").appendChild(li);
+    }
     document.getElementById("input").value = "";
 
     const span = document.createElement("span");
     const txt = document.createTextNode("\u00D7");
+
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
@@ -51,9 +65,7 @@ function addToList() {
         }
     }
 
-    // Локальный сторож работать не хочет
-    //localStorage.setItem('span', value);
-    //localStorage.getItem(span);
+    updateCounters();
 }
 
 // Показать все дела
@@ -61,8 +73,8 @@ function showAll() {
     const todoList = document.getElementsByTagName("li");
 
     for (let i = 0; i < todoList.length; i++) {
-        if (todoList.item(i).className === 'hide') {
-            todoList.item(i).className = '';
+        if (todoList.item(i).classList.contains('hide')) {
+            todoList.item(i).classList.remove('hide');
         }
     }
 }
@@ -72,8 +84,11 @@ function showChecked() {
     const todoList = document.getElementsByTagName("li");
 
     for (let i = 0; i < todoList.length; i++) {
-        if (todoList.item(i).classList.value !== 'checked') {
-            todoList.item(i).className = 'hide';
+        if (todoList.item(i).classList.contains('checked')) {
+            todoList.item(i).classList.remove('hide');
+        }
+        else {
+            todoList.item(i).classList.add('hide');
         }
     }
 }
@@ -83,8 +98,25 @@ function showUnfinished() {
     const todoList = document.getElementsByTagName("li");
 
     for (let i = 0; i < todoList.length; i++) {
-        if (todoList.item(i).classList.value === 'checked') {
-            todoList.item(i).className = 'hide';
+        if (todoList.item(i).classList.contains ('checked')) {
+            todoList.item(i).classList.add('hide');
+        }
+        else {
+            todoList.item(i).classList.remove('hide');
         }
     }
 }
+
+function updateCounters() {
+    const allCount = document.getElementById('allCount');
+    let doneCount = document.getElementById('doneCount');
+    let notDoneCount = document.getElementById('notDoneCount');
+    let allNum = document.querySelectorAll('li').length.toString();
+    let doneNum = document.querySelectorAll('li.checked').length.toString()
+    let notDoneNum = allNum - doneNum;
+
+    allCount.innerText = allNum;
+    doneCount.innerText = doneNum;
+    notDoneCount.innerText = notDoneNum.toString();
+}
+
